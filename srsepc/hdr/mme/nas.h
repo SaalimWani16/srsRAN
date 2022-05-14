479128cc144cc14de54c34f7a48ff76894c1e056
Saalim <saalimwani@gmail.com>
2022-05-14 21:22:46 +0200

    revised patch file IMSI

--- srsepc/hdr/mme/nas.h
+++ srsepc/hdr/mme/nas.h
@@ -161,6 +161,11 @@ public:
    * Initial UE messages *
    ***********************/
   // Attach request messages
+  static bool handle_attach_request_IMSI_C(uint32_t                enb_ue_s1ap_id,
+                                    struct sctp_sndrcvinfo* enb_sri,
+                                    srsran::byte_buffer_t*  nas_rx,
+                                    const nas_init_t&       args,
+                                    const nas_if_t&         itf);
   static bool handle_attach_request(uint32_t                enb_ue_s1ap_id,
                                     struct sctp_sndrcvinfo* enb_sri,
                                     srsran::byte_buffer_t*  nas_rx,
